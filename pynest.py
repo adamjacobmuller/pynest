29268cda4a17daa5ab3624a885da2911e3f06118
Adam Jacob Muller <adam@isprime.com>
2013-05-07 00:15:36 -0400

    cli control mode of the script

--- pynest.py
+++ pynest.py
@@ -3,6 +3,7 @@ import urllib
 import json
 import pprint
 import time
+import sys
 
 
 def j_dump(data):
@@ -18,7 +19,9 @@ class NestThermostat():
         self.heat = thermostat['hvac_heater_state']
         self.serial = serial
         self.structure = structure
-        data['track'][serial]
+        self.track = data['track'][serial]
+        self.shared = data['shared'][serial]
+        self.device = data['device'][serial]
 
     def __repr__(self):
         return "<NestThermostat %s / Serial %s [ Temperature: %s / Heat: %s / Cool: %s / Fan: %s ]>" % (
@@ -38,7 +41,7 @@ class NestThermostat():
 
     def set_temperature(self, temperature):
         # temp in c
-        return self.set_thermostat_shared(target_change_pending = True, target_temperature = temperature)
+        return self.set_thermostat_shared(target_change_pending = True, target_temperature = int(temperature))
 
     def set_mode(self, mode):
         # heat|cool|range|off
@@ -46,11 +49,11 @@ class NestThermostat():
 
     def set_fan_timer_timeout(self, timeout):
         # epoch seconds, when to turn off fan
-        return self.set_thermostat_device(fan_timer_timeout = timeout)
+        return self.set_thermostat_device(fan_timer_timeout = int(timeout))
 
     def set_fan_timer_duration(self, duration):
         # seconds
-        return self.set_thermostat_device(fan_timer_duration = duration)
+        return self.set_thermostat_device(fan_timer_duration = int(duration))
 
     def set_fan_duty_cycle(self, duty_cycle):
         # seconds per hour
@@ -58,10 +61,10 @@ class NestThermostat():
 
     def set_fan_duty_range(self, start, end):
         # seconds
-        return self.set_thermostat_device(fan_duty_start_time = start, fan_duty_end_time = end)
+        return self.set_thermostat_device(fan_duty_start_time = int(start), fan_duty_end_time = int(end))
 
     def set_fan_mode(self, mode):
-        # on|off|auto|duty-cycle
+        # on|auto|duty-cycle
         return self.set_thermostat_device(fan_mode = mode)
 
 
@@ -71,10 +74,10 @@ class NestStructure():
         self.name = structure['name']
         self.away = structure['away']
         self.uuid = uuid
-        self.thermostats = []
+        self.thermostat_list = []
         for device in structure['devices']:
             serial = device[7:]
-            self.thermostats.append(NestThermostat(self, data['shared'][serial], data, serial))
+            self.thermostat_list.append(NestThermostat(self, data['shared'][serial], data, serial))
 
     def set_structure(self, **kwargs):
         return self.account._make_request("/v2/put/structure." + self.uuid, data = kwargs)
@@ -92,8 +95,25 @@ class NestStructure():
             self.away
         )
 
+    def thermostats(self, **kwargs):
+        try:
+            if kwargs['all'] is True:
+                return self.thermostat_list
+        except KeyError:
+            pass
+        m_thermostats = []
+        for thermostat in self.thermostat_list:
+            match = True
+            for arg in kwargs:
+                if getattr(thermostat, arg) != kwargs[arg]:
+                    match = False
+                    break
+            if match is True:
+                m_thermostats.append(thermostat)
+        return m_thermostats
+
     def thermostat(self, **kwargs):
-        for thermostat in self.thermostats:
+        for thermostat in self.thermostat_list:
             match = True
             for arg in kwargs:
                 if getattr(thermostat, arg) != kwargs[arg]:
@@ -126,8 +146,25 @@ class NestAccount():
         self.access_token = self.res["access_token"]
         self.userid = self.res["userid"]
 
+    def structures(self, **kwargs):
+        try:
+            if kwargs['all'] is True:
+                return self.structure_list
+        except KeyError:
+            pass
+        m_structures = []
+        for structure in self.structure_list:
+            match = True
+            for arg in kwargs:
+                if getattr(structure, arg) != kwargs[arg]:
+                    match = False
+                    break
+            if match is True:
+                m_structures.append(structure)
+        return m_structures
+
     def structure(self, **kwargs):
-        for structure in self.structures:
+        for structure in self.structure_list:
             match = True
             for arg in kwargs:
                 if getattr(structure, arg) != kwargs[arg]:
@@ -138,9 +175,9 @@ class NestAccount():
 
     def load(self):
         req = self._make_request("/v2/mobile/user.%s" % self.userid)
-        self.structures = []
+        self.structure_list = []
         for structure in req['structure']:
-            self.structures.append(NestStructure(self, req['structure'][structure], req, uuid = structure))
+            self.structure_list.append(NestStructure(self, req['structure'][structure], req, uuid = structure))
 
     def _make_request(self, url, data = None, headers = {}):
         if self.res is None:
@@ -178,8 +215,32 @@ class NestAccount():
         return "<NestAccount %s / UserID %s>" % (self.username, self.userid)
 
 
+def get_args():
+    from optparse import OptionParser
+    parser = OptionParser()
+    parser.add_option("-s", "--structure", dest="structures", action="append", default = [])
+    parser.add_option("-t", "--thermostat", dest="thermostats", action="append", default = [])
+    parser.add_option("-u", "--all-thermostats", dest="all_thermostats", action="store_true", default = False)
+    parser.add_option("-w", "--all-structures", dest="all_structures", action="store_true", default = False)
+    parser.add_option("-d", "--debug", dest="debug", action="store_true", default = False)
+    parser.add_option("-l", "--list", dest="list", action="store_true", default = False)
+
+    return parser.parse_args()
+
+
+def list_to_dicts(items):
+    r_list = []
+    for item in items:
+        r_dict = {}
+        for s_item in item.split(","):
+            (k, v) = item.split("=", 2)
+            r_dict[k] = v
+        r_list.append(r_dict)
+    return r_list
+
 if __name__ == "__main__":
-    if False:
+    (options, args) = get_args()
+    if options.debug:
         handler = urllib2.HTTPSHandler(debuglevel=1)
         handler.set_http_debuglevel(1)
         opener = urllib2.build_opener(handler)
@@ -188,19 +249,73 @@ if __name__ == "__main__":
     settings = json.load(open("pynest.json", "r"))
 
     nest = NestAccount(settings['username'], settings['password'])
-    for structure in ['Home']:
-        for thermostat in ['Upstairs', 'Downstairs']:
-            nest.structure(name=structure).thermostat(name=thermostat).set_fan_duty_cycle(900)
-            nest.structure(name=structure).thermostat(name=thermostat).set_fan_duty_range(60 * 60 * 8, 60 * 60 * 23)
-            nest.structure(name=structure).thermostat(name=thermostat).set_fan_mode("duty-cycle")
-    #print nest.structure(name="Home").thermostat(name="Upstairs").set_fan_duty_cycle(900)
-    #print nest.structure(name="Home").thermostat(name="Downstairs").set_fan("on")
-    #print nest.structure(name="Home").thermostat(name="Downstairs").set_temperature(27)
-    #print nest.structure(name="Home").thermostat(name="Basement").set_temperature(10)
-    print nest
-    for structure in nest.structures:
-        print "    %s" % structure
-        for thermostat in structure.thermostats:
-            print "        %s" % thermostat
-        #    thermostat.set_temperature(27)
-        #structure.set_back()
+
+    if options.list:
+        print nest
+        for structure in nest.structures():
+            print "    %s" % structure
+            for thermostat in structure.thermostats():
+                print "        %s" % thermostat
+        sys.exit(0)
+
+    if options.all_structures:
+        control = "structures"
+        structures = [ {'all': True}]
+    elif len(options.structures) > 0:
+        control = "structures"
+        structures = list_to_dicts(options.structures)
+
+    if options.all_thermostats:
+        control = "thermostats"
+        thermostats = [ {'all': True}]
+    elif len(options.thermostats) > 0:
+        control = "thermostats"
+        thermostats = list_to_dicts(options.thermostats)
+
+    thermostat_command_map = {
+        'temperature': ( 'set_temperature', 1),
+        'fan-duty-cycle': ( 'set_fan_duty_cycle', 1),
+        'fan-mode': ( 'set_fan_mode', 1),
+        'fan-duty-range': ( 'set_fan_duty_range', 2),
+        'fan-duty-cycle': ( 'set_fan_duty_cycle', 1),
+        'fan-timer-duration': ( 'set_fan_timer_duration', 1),
+        'fan-timer-timeout': ( 'set_fan_timer_timeout', 1),
+        'mode': ( 'set_mode', 1)
+    }
+
+    structure_command_map = {
+        'away': ( 'set_away', 0),
+        'back': ( 'set_back', 0)
+    }
+
+    nest_controls = []
+    if control == "thermostats":
+        command_map = thermostat_command_map
+        for structure_kwargs in structures:
+            for structure in nest.structures(**structure_kwargs):
+                for thermostat_kwargs in thermostats:
+                    for thermostat in structure.thermostats(**thermostat_kwargs):
+                        nest_controls.append(thermostat)
+    if control == "structures":
+        command_map = structure_command_map
+        for structure_kwargs in structures:
+            for structure in nest.structures(**structure_kwargs):
+                nest_controls.append(structure)
+
+    print "Controlling:"
+    for nest_control in nest_controls:
+        print "    %s" % nest_control
+
+    args_s = list(args)
+    for nest_control in nest_controls:
+        args = list(args_s)
+        while len(args) > 0:
+            command = args.pop(0)
+            function = command_map[command]
+            method = getattr(nest_control, function[0])
+            function_args = []
+            for i in range(0, function[1]):
+                function_args.append(args.pop(0))
+
+            method(*function_args)
+            print "%s(%s)" % (method, function_args)
